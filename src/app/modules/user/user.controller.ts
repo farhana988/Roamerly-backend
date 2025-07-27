1132b27a94f8be70ea3dea2b60878fc27b666541
farhana988 <farhana744255@gmail.com>
2025-07-27 10:38:53 +0600

    first commit

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes"
+import { UserServices } from "./user.service";
+
+// create a user
+const createUser = async (req:Request, res :Response, next: NextFunction )=>{
+    try {
+      const user =await UserServices.createUser(req.body)
+        res.status(httpStatus.CREATED).json({
+            message: "User Created Successfully", 
+            user
+        })
+    }catch(err:any){
+        console.log(err)
+        next(err)
+    }
+}
+
+
+
+export const UserControllers ={
+    createUser,
+    
+}
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,24 @@
+import { IUser } from "./user.interfaace";
+import { User } from "./user.model";
+
+
+const createUser = async (payload: Partial<IUser>)=>{
+      const {name, email} =payload
+        const user = await User.create({
+            name,
+            email
+        })
+        return user
+}
+
+const getAllUsers = async () =>{
+    const users = await User.find({})
+    return users 
+}
+
+
+
+export const UserServices ={
+    createUser,
+    getAllUsers
+}
